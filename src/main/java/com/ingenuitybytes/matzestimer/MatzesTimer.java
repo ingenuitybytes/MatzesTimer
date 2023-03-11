a9d11464b6cdc93430aa4e22b9b7253d5585409f
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2023-03-11 02:56:04 +0100

    Update project

--- src/main/java/com/ingenuitybytes/matzestimer/MatzesTimer.java
+++ src/main/java/com/ingenuitybytes/matzestimer/MatzesTimer.java
@@ -1,34 +1,60 @@
 package com.ingenuitybytes.matzestimer;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
-import net.minecraftforge.common.MinecraftForge;
 
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import net.minecraftforge.fml.client.config.GuiConfig;
 
 @Mod(modid = MatzesTimer.MODID, version = MatzesTimer.VERSION)
 public class MatzesTimer {
     public static final String MODID = "matzestimer";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
+    public static final String CATEGORY_MYMOD = "Matzes Timer";
 
-    private static final KeyBinding KEY_TIMER = new KeyBinding("Starte 2 Minuten Timer", Keyboard.KEY_N, "key.categories.gameplay");
+    private static final KeyBinding KEY_TIMER = new KeyBinding("Starte den Timer", Keyboard.KEY_N,
+            CATEGORY_MYMOD);
+    private static final KeyBinding KEY_CONFIG = new KeyBinding("Öffne das Config Menü", Keyboard.KEY_C,
+            CATEGORY_MYMOD);
 
+    private static int TIMER_DURATION = 120; // 2 minutes in seconds
 
+    public static Configuration config;
     private static Timer timer;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         ClientRegistry.registerKeyBinding(KEY_TIMER);
+        ClientRegistry.registerKeyBinding(KEY_CONFIG);
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Load and save the configuration file
+        File configFile = new File(Minecraft.getMinecraft().mcDataDir, "config/" + MODID + ".cfg");
+        config = new Configuration(configFile);
+        config.load();
+        int defaultDuration = 120; // 2 minutes in seconds
+        int duration = config.getInt("Timer Zeit", Configuration.CATEGORY_GENERAL, defaultDuration, 10, 3600,
+                "The duration of the timer in seconds (between 10 and 3600)");
+        config.addCustomCategoryComment(CATEGORY_MYMOD, "Keybinds für Matzes Timer");
+        config.save();
+
+        // Update the timer duration
+        TIMER_DURATION = duration;
     }
 
     @SubscribeEvent
@@ -36,6 +62,19 @@ public class MatzesTimer {
         if (KEY_TIMER.isPressed()) {
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Der Timer wurde gestartet!"));
             startTimer();
+        } else if (KEY_CONFIG.isPressed()) {
+            Minecraft.getMinecraft().displayGuiScreen(new MatzesTimerConfigGui(Minecraft.getMinecraft().currentScreen));
+        }
+    }
+
+    public static class MatzesTimerConfigGui extends GuiConfig {
+        public MatzesTimerConfigGui(GuiScreen parentScreen) {
+            super(parentScreen,
+                    new ConfigElement(config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                    MODID,
+                    false,
+                    false,
+                    GuiConfig.getAbridgedConfigPath(config.toString()));
         }
     }
 
@@ -45,10 +84,22 @@ public class MatzesTimer {
         }
         timer = new Timer();
         timer.schedule(new TimerTask() {
+            int count = TIMER_DURATION;
+
             @Override
             public void run() {
-                Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("2 Minuten sind um!"));
+                if (count > 10) {
+                    count--;
+                } else if (count > 0) {
+                    count--;
+                    Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
+                            EnumChatFormatting.RED + String.format("Noch %d Sekunden übrig!", count)));
+                } else {
+                    timer.cancel();
+                    Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Der Timer ist um!"));
+                    config.save(); // Speichert die Konfigurationsdatei
+                }
             }
-        }, 2 * 60 * 1000); // 2 Minuten in Millisekunden
+        }, 0, 1000); // 1 second delay between each count
     }
 }
